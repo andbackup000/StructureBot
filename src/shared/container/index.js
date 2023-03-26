eb4c308f2c9ebbdfd1c7946f97710bd082ad90b9
andbackup000 <and.backup000@gmail.com>
2023-03-26 21:54:08 +0000

    "feat: dist build"

--- src/shared/container/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-const TradesRepository_1 = require("../../modules/tradesOperations/repositories/implametations/TradesRepository");
-const UserRepository_1 = require("../../modules/users/repositories/implametations/UserRepository");
-tsyringe_1.container.registerSingleton("UserRepository", UserRepository_1.UserRepository);
-tsyringe_1.container.registerSingleton("TradesRepository", TradesRepository_1.TradesRepository);
